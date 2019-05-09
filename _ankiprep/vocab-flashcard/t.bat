ab57194bcdf4c5377b418fbc96ad0456f67572a6
Alex <apojoga@gmail.com>
2019-05-09 04:42:18 +0300

    organize vocab-flashcard/t in the same way as kanji-flashcard/t

--- _ankiprep/vocab-flashcard/t.bat
+++ _ankiprep/vocab-flashcard/t.bat
@@ -2,6 +2,8 @@
 rem used for quickly testing changes to flashcard html/css/js
 rem generates files for a dummy test page and displays it in firefox
 
+cd t
+
 ruby gen.rb
 
-start firefox __test\flashcard.html
+start firefox __OUT__\flashcard.html
