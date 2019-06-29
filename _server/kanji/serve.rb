4f3217c9a4b41e9cd607077d8b263caab69096a0
Alex <apojoga@gmail.com>
2019-06-29 21:46:08 +0300

    fix kanji/server.rb: was managing chunk lifetimes incorrectly; wasn't freeing chunks upon being used

--- _server/kanji/serve.rb
+++ _server/kanji/serve.rb
@@ -5,12 +5,9 @@ $RIKAICHAN_TXT = "D:/_rikaichan.txt"
 
 module Kanji
 
-  KanjiData = Struct.new(:time, :chunks)
-
-  @data = Hash.new
-
   def self.serve(url, s)
 
+    @data ||= {}
     @data.delete_if {|k,v| (Time.now - v.time) > 10}
 
     if m = url.match(/^kanji\/([0-9a-fA-F]{4})$/)
@@ -21,37 +18,31 @@ module Kanji
 
     elsif m = url.match(/^kanji\/(set|copy)\/([0-9a-fA-F]{4})\/(\d+)\/(\d+)\/(.+)$/)
       op, utf16, cur, tot, chunk = m[1], m[2], m[3], m[4], m[5]
-      if op == "copy" && @last_copy
-        elapsed = (Time.now - @last_copy).to_i
-        if (elapsed < 1)
-          s.print "HTTP/1.1 200/OK\r\n"
-          s.print "Content-type: text/html\r\n\r\n"
-          s.print "too early"
-          puts "copy aborted (within 1s of last; prevent double click)"
-          return
-        end
-      end
-      unless @data.has_key? utf16
-        @data[utf16] = KanjiData.new(Time.now, [nil] * tot.to_i)
+      s.print "HTTP/1.1 200/OK\r\n"
+      s.print "Content-type: text/html\r\n\r\n"
+      @data[utf16] ||= Struct.new(:time, :chunks).new(Time.now, [nil] * tot.to_i)
+      if @data[utf16].chunks[cur.to_i - 1] != nil
+        puts "chunk aborted! (chunk #{cur}/#{tot} was already present)"
+        @data.delete utf16
+        return
       end
       @data[utf16].chunks[cur.to_i - 1] = chunk
       @data[utf16].time = Time.now
       puts "[got chunk #{cur}/#{tot} for #{utf16}]"
-      if op == "copy"
-        s.print "HTTP/1.1 200/OK\r\n"
-        s.print "Content-type: text/plain\r\n\r\n"
-        if @data[utf16].chunks.all? {|c| c != nil}
-          data = @data[utf16].chunks.join
-          result = `copytocb.exe #{data}`
-          puts result
-          s.print result
+      if op == "copy" && @data[utf16].chunks.all? {|c| c != nil}
+        if @last_copy_time && (Time.now - @last_copy_time) < 0.5
+          puts "chunk aborted! (within 0.5s of last; prevent double click)"
           @data.delete utf16
-          puts "running `AutoIt3.exe ankipaste.au3`"
-          puts `AutoIt3.exe ankipaste.au3`
-          @last_copy = Time.now
-        else
-          s.print "moar"
+          return
         end
+        data = @data[utf16].chunks.join
+        result = `copytocb.exe #{data}`
+        puts result
+        s.print result
+        puts "running `AutoIt3.exe ankipaste.au3`"
+        puts `AutoIt3.exe ankipaste.au3`
+        @last_copy_time = Time.now
+        @data.delete utf16
       end
 
     elsif m = url.match(/^kanji\/show\/([0-9a-fA-F]{4})$/)
@@ -76,7 +67,7 @@ module Kanji
         if dir == "flashcard" && prefix == "k"
           data_override = CGI.unescape( @data[utf16].chunks.join )
           html.sub!("var _data_override = null", "var _data_override = #{data_override}")
-#          @data.delete utf16
+          @data.delete utf16
         end
         s.print "HTTP/1.1 200/OK\r\n"
         s.print "Content-type: text/html\r\n\r\n"
