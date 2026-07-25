7084e83919a3b0a428ff52b488f6b681d3d502da
Alexandru Pojoga <apojoga@gmail.com>
2026-07-25 15:15:28 +0200

    audio.marshal

--- _ankiprep/vocab/audio.rb
+++ _ankiprep/vocab/audio.rb
@@ -1,16 +1,16 @@
 ﻿
 module Vocab; module Audio
 
-  if File.exist? 'vocab/mp3list.marshal'
-    print "[Vocab::Audio] loading mp3 list... "
-    @mp3list = File.open('vocab/mp3list.marshal', 'rb') {|f| Marshal.load(f)}
+  print "[Vocab::Audio] loading mp3 list... "
+  if File.exist? "#{$RES_DIR}/.marshal/audio.marshal"
+    @mp3list = File.open("#{$RES_DIR}/.marshal/audio.marshal", 'rb') {|f| Marshal.load(f)}
   else
-    print "[Vocab::Audio] getting mp3 list... "
-    `cmd /u /c dir /b D:\\Japanese\\_dict\\audio > vocab\\mp3list.tmp`
+    win_path = "#{$RES_DIR}/audio".gsub('/','\\')
+    `cmd /u /c dir /b #{win_path} > vocab\\mp3list.tmp`
     @mp3list = File.read('vocab/mp3list.tmp', mode:'r:UTF-16LE:UTF-8').split("\n").to_set
     FileUtils.rm 'vocab/mp3list.tmp'
-    print "saving... "
-    File.open('vocab/mp3list.marshal', 'wb') {|f| Marshal.dump(@mp3list, f)}
+    print "marshaling... "
+    File.open("#{$RES_DIR}/.marshal/audio.marshal", 'wb') {|f| Marshal.dump(@mp3list, f)}
   end
   puts "#{@mp3list.size} files"
 
